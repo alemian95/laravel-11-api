7ae0d1f19c76edf98d05c1a595863d965951e6e3
Alessandro Mian <alessandromian95@gmail.com>
2024-06-26 00:38:48 +0200

    Locales ITA

---
+++ lang/it/form.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'login' => [
+        'title' => 'Login',
+        'email' => 'Email',
+        'password' => 'Password',
+        'remember_me' => 'Ricorda questo dispositivo',
+        'submit' => 'Login',
+    ],
+    'forgot_password' => [
+        'title' => 'Dimenticato la password?',
+        'description' => 'Inserisci il tuo indirizzo email nel form, ti verrà inviato un link per reimpostarla. Segui le istruzioni per eseguire la procedura di ripristino.',
+        'email' => 'Email',
+        'submit' => 'Ho dimenticato la password, inviami un link per reimpostarla',
+    ],
+
+];
---
+++ lang/it/passwords.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'La tua password è stata reimpostata.',
+    'sent' => 'Ti abbiamo inviato un email con il link per reimpostare la password.',
+    'not_verified' => 'Il tuo account non è ancora stato verificato. Controlla la tua casella di posta',
+    'resend_verification_email' => 'Invia nuovamente l\'email di verifica',
+    'throttled' => 'Troppi tentativi, attendi prima di riprovare.',
+    'token' => 'Il link non è valido.',
+    'user' => "Nessun utente registrato con questo indirizzo email.",
+    'forgot_submit_form' => 'Password dimenticata',
+
+];
